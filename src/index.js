de7334e6377e7c09f5dfd5a41343f99c688c5609
Nick3SK <skopin.nicita654@yandex.ru>
2023-03-10 11:41:53 +0300

    add game

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import * as ReactDomClient from 'react-dom/client'
-import App from "./App";
-import "./stylish.css"
-
+import Menu from "./Menu";
+import { BrowserRouter } from 'react-router-dom';
 const app = ReactDomClient.createRoot(document.getElementById('app'));
 app.render(
+<BrowserRouter>
+    <Menu />
+</BrowserRouter>
 
-    <App />
 );
 
